9fd49c9b6c8105446f2742b37b35a627ee16e5b3
Dimex19 <azeezabolaji06gmail.com>
2025-05-30 01:05:42 +0100

    about section added

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -15,7 +15,7 @@ const ProjectCard: React.FC<projectCardProps> = ({image, title, desc, proj}) => 
         <div className="p-[18px]">
             <p className='text-[#F8007F] text-[20px] font-medium'>{proj}</p>
             <p className="text-[26px] font-medium">{title}</p>
-            <p className="text-[16px] font-medium w-[349px]">{desc}</p>
+            <p className="text-[16px] font-medium md:w-[349px]">{desc}</p>
             <Button text='See the Impact' style='mr-auto ml-auto border-2 border-[#DA1077] text-[#DA1077] py-2 rounded-[8px] mt-5 w-[240px] text-center'/>
         </div>
     </div>
